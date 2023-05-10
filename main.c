0634978e39cda6cd7f159c7be051d69aa71abf2e
ErfanDehghani <erfan.dehghani67@yahoo.com>
2023-05-11 03:20:59 +0430

    output is colored now

--- main.c
+++ main.c
@@ -1,28 +1,29 @@
 #include "headers/headers.h"
-#define MAX_CHAR_INPUT_SIZE 250
-
-//Getting input
-int getLine(char* s, int lim) {
-    int c, i;
-    for (i = 0; i < lim && (c = getchar()) != EOF && c != '\n'; i++)
-        s[i] = c;
-    s[i] = '\0';
-    return i - 1;
-}
 
 int main()
 {
+    system("cls");
+    printf("\n");
+
     float result;
 
 	char inputString[MAX_CHAR_INPUT_SIZE];
+	char *Seprator = "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n";
 
-	while (getLine(inputString, MAX_CHAR_INPUT_SIZE))
+	while (1)
 	{
         char outputString[MAX_CHAR_INPUT_SIZE];
+
+        printf_s("\033[0;36mEnter something to calculate: \033[0;37m");
+        getLine(inputString, MAX_CHAR_INPUT_SIZE);
+
 		evaluateStringToPostfix( inputString, outputString);
 		printf("Postfix string is: %s\n", outputString);
+
         result = reversePolishCalculator(outputString);
-		printf("Result is: %f\n", result);
+		printf("\033[0;32mResult is: %f\n \033[0;37m", result);
+
+        printf_s("\n%s\n",Seprator);
 	}
 	
 }
